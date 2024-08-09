0736263d46d8b84c88a0b594b85042f84eed9259
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2024-08-09 10:58:17 -0400

    fix: incorrect JSON tags in page info

--- models_common.go
+++ models_common.go
@@ -1,10 +1,10 @@
 package vanta
 
 type PageInfo struct {
-	HasPreviousPage bool   `json:"has_previous_page"`
-	HasNextPage     bool   `json:"has_next_page"`
-	StartCursor     string `json:"start_cursor"`
-	EndCursor       string `json:"end_cursor"`
+	HasPreviousPage bool   `json:"hasPreviousPage"`
+	HasNextPage     bool   `json:"hasNextPage"`
+	StartCursor     string `json:"startCursor"`
+	EndCursor       string `json:"endCursor"`
 }
 
 type GenericNamedItem struct {
